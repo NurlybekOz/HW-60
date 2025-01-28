69ac0ab68c20c6fdc3dad168af2c31d1b05ae94c
nurlybek <orozovnurlybek@gmail.com>
2025-01-28 15:18:11 +0600

    Импортировал все компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-
 import './App.css'
+import UserForm from "./components/UserForm/UserForm.tsx";
+import UserMessage from "./components/UserMessage/UserMessage.tsx";
 
 const App = () => {
+    return (
+        <div className='container'>
+            <div className='chat' style={{width: '600px'}}>
+                <UserForm></UserForm>
+                <hr/>
+                <UserMessage></UserMessage>
+            </div>
+        </div>
 
+    )
 };
 
 export default App
